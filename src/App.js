99839762bc0a5ab206ca90d3f1025660411527ee
Max <Max.95hansen@gmail.com>
2022-07-18 14:35:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dogImage: 'temp'
+    };
+
+    this.getDog = this.getDog.bind(this);
+  }
+
+  getDog() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => this.setState({ dogImage: data.message}))
+  }
+
+  componentDidMount() {
+    this.getDog();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from Dog!</h1>
+        <img src={this.state.dogImage} alt="Dog image"/>
+        <button onClick={this.getDog}>
+          Give me Another!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
